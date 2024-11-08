adc2f177d4b99aea13c2fd4bcbced7b797645b3b
rozzy <rosekaremeri@gmail.com>
2024-11-08 14:27:36 +0300

    fixxes

--- craft_sync/src/app/api/auth/signup/route.ts
+++ craft_sync/src/app/api/auth/signup/route.ts
@@ -3,19 +3,46 @@ import prisma from '../../utils/prisma';
 import { hash } from 'bcryptjs';
 import { NextResponse } from 'next/server';
 
+// export async function POST(req: Request) {
+//   try {
+//     const { email, password, role } = await req.json();
+
+//     // Check if all required fields are present
+//     if (!email || !password || !role) {
+//       return NextResponse.json({ message: 'Missing required fields' }, { status: 400 });
+//     }
+
+//     // Hash the password
+//     const hashedPassword = await hash(password, 10);
+
+//     // Attempt to create the user
+//     const newUser = await prisma.user.create({
+//       data: {
+//         email,
+//         password: hashedPassword,
+//         role,
+//       },
+//     });
+
+//     return NextResponse.json({ message: 'User created successfully' }, { status: 201 });
+//   } catch (error) {
+//     console.error('Error creating user:', error);
+//     return NextResponse.json({ message: 'Error creating user', error }, { status: 500 });
+//   }
+// }
+
 export async function POST(req: Request) {
+  const { email, password, role } = await req.json();
+
+  if (!email || !password || !role) {
+    return NextResponse.json({ message: 'Missing required fields' }, { status: 400 });
+  }
+
+  const { default: prisma } = await import('../../utils/prisma');
+
+  const hashedPassword = await hash(password, 10);
+
   try {
-    const { email, password, role } = await req.json();
-
-    // Check if all required fields are present
-    if (!email || !password || !role) {
-      return NextResponse.json({ message: 'Missing required fields' }, { status: 400 });
-    }
-
-    // Hash the password
-    const hashedPassword = await hash(password, 10);
-
-    // Attempt to create the user
     const newUser = await prisma.user.create({
       data: {
         email,
@@ -23,7 +50,6 @@ export async function POST(req: Request) {
         role,
       },
     });
-
     return NextResponse.json({ message: 'User created successfully' }, { status: 201 });
   } catch (error) {
     console.error('Error creating user:', error);
